90aa0730be28142862732c84843628aeb0b24261
Ronaldo Gonçalves <rogdsilva@rd.com.br>
2025-09-05 01:16:35 -0300

    feat(HaVagas) : Adiciona data de nascmento, sexo e formação ao formulário

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/havagas/MainActivity.kt
@@ -2,6 +2,8 @@ package br.edu.ifsp.scl.ads.prdm.sc3015467.havagas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import br.edu.ifsp.scl.ads.prdm.sc3015467.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityMainBinding.telefoneSp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position > 0) {
+                    activityMainBinding.telefoneCelularLl.visibility = View.VISIBLE
+                } else {
+                    activityMainBinding.telefoneCelularLl.visibility = View.GONE
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                activityMainBinding.telefoneCelularLl.visibility = View.GONE
+            }
+        }
     }
 }
